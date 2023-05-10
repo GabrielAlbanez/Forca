09941ac60fac7856c6f63ca32ff5e3a82a4a43cb
GABRIEL GONCALVES ALBANEZ <gabriel.galbanez@senacsp.edu.br>
2023-05-10 17:10:33 -0300

    8att

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ chk.addEventListener("change", () => {
 });
 
 chk.addEventListener("change", () => {
-  console.log(button.classList[1]);
   const buttons = document.getElementsByTagName("button");
   for (let i = 0; i < buttons.length; i++) {
     let button = buttons[i];
@@ -60,10 +59,15 @@ for (var contador = 0; contador < 14; contador++) {
 
   button.setAttribute("value", keys[contador]);
   button.setAttribute("class", "btn btn-outline-dark a");
-
+  
+  button.setAttribute("id", keys[contador]);
+ button.setAttribute("onclick", "getValueBotao(this.id)");
   button.innerHTML = keys[contador];
+ 
   button.style.width = "40px";
 }
+
+
 
 for (var contador = 14; contador < 26; contador++) {
   var divb = document.createElement("div");
@@ -79,95 +83,86 @@ for (var contador = 14; contador < 26; contador++) {
   button.style.width = "40px";
 }
 
+var words = [
+  "abelha",
+  "musculo",
+  "piscina",
+  "turma da monica",
+  "the wichter",
+  "friends",
+  "Apenas Um Show",
+  "turma da monica",
+  "Jão Pessoa",
+  "github",
+  "murder in my mind",
+  "land of the fire",
+  "Pirates",
+  "Lazaro Talarico",
+  "Ocean",
+  "Terra",
+  "Montanha",
+];
 
+var contwords = words.length;
+var drawnword;
+var wordtype;
+var wordcount;
+var word = [];
 
-
-
-
-
-
-  var words = [
-    "abelha",
-    "musculo",
-    "piscina",
-    "turma da monica",
-    "the wichter",
-    "friends",
-    "Apenas Um Show",
-    "turma da monica",
-    "Jão Pessoa",
-    "João e Maria Caçador de Bruxas",
-    "github",
-    "murder in my mind",
-    "land of the fire",
-    "Pirates",
-    "Lazaro Talarico",
-    "Ocean",
-    "Terra",
-    "Montanha",
-  ];
-
-
-  var contwords = words.length
-var drawnword
-var wordtype
-var wordcount
-var word = []
-
-
-  function prize(){
-    
-    var diff = contwords
-    var rand = Math.random()
-    rand = Math.floor(rand * diff)
-    return rand
-
+function prize() {
+  var diff = contwords;
+  var rand = Math.random();
+  rand = Math.floor(rand * diff);
+  return rand;
 }
 
-drawnword = words[prize()]
+drawnword = words[prize()];
 
-console.log(drawnword)
+var wordspace = drawnword.split(" ");
 
-var wordspace = drawnword.split(' ')
+wordcount = wordspace.length;
 
-wordcount = wordspace.length
-
-console.log(wordcount)
-
-if (wordcount > 1){
-    wordtype = true
-}
-else {
-    wordtype = false
-}
-
-
-if (wordtype == true){
-    for(var i = 0; i < wordcount; i++){
-        word[i] = wordspace[i].split('')
-        console.log(wordspace[i])
-        console.log(word[i])
-    }
+if (wordcount > 1) {
+  wordtype = true;
 } else {
-    word[0] = wordspace[0].split('')
+  wordtype = false;
 }
 
-for(var i=0; i < word.length; i++){
-    for (var j=0; j < word[i].length; j++){
-        var input = document.createElement('input')
-        input.setAttribute('type','text')
-        input.setAttribute('class', 'form-control')
-        document.getElementById('word').appendChild(input)
-   }
+if (wordtype == true) {
+  for (var i = 0; i < wordcount; i++) {
+    word[i] = wordspace[i].split("");
+  }
+} else {
+  word[0] = wordspace[0].split("");
 }
 
+for (var i = 0; i < word.length; i++) {
+  for (var j = 0; j < word[i].length; j++) {
+    var input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("class", "form-control");
+    input.setAttribute("value", word[i][j]);
+    input.setAttribute("id", "input");
+    input.style.color = "white";
+    document.getElementById("word").appendChild(input);
 
+    
+  }
+  if (word[i].length > 1) {
+    var espaco = document.createElement("div");
+    espaco.setAttribute("class", "espaco");
+    document.getElementById("word").appendChild(espaco);
 
-
-
-
-
-
-
+  }
+}
 
 // document.getElementById("in").innerHTML = resultados
+
+var letter
+
+function getValueBotao(id){
+  // letter = document.getElementById("").innerHTML
+  var valueButton = id
+  alert(valueButton)
+ 
+}
