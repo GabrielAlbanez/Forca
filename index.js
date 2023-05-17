7a4958127ce4de904cc37dcad3aa7e9c690693d4
GABRIEL GONCALVES ALBANEZ <gabriel.galbanez@senacsp.edu.br>
2023-05-17 17:26:13 -0300

    att

--- index.js
+++ index.js
@@ -20,32 +20,32 @@ chk.addEventListener("change", () => {
 });
 
 const keys = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 var letters = keys.length;
@@ -59,15 +59,13 @@ for (var contador = 0; contador < 14; contador++) {
 
   button.setAttribute("value", keys[contador]);
   button.setAttribute("class", "btn btn-outline-dark a");
-  
+
   button.setAttribute("id", keys[contador]);
- button.setAttribute("onclick", "getValueBotao(this.id)");
+  button.setAttribute("onclick", "getValueBotao(this.id)");
   button.innerHTML = keys[contador];
- 
+
   button.style.width = "40px";
 }
-
-
 
 for (var contador = 14; contador < 26; contador++) {
   var divb = document.createElement("div");
@@ -78,6 +76,8 @@ for (var contador = 14; contador < 26; contador++) {
 
   button.setAttribute("value", keys[contador]);
   button.setAttribute("class", "btn btn-outline-dark a");
+  button.setAttribute("id", keys[contador]);
+  button.setAttribute("onclick", "getValueBotao(this.id)");
 
   button.innerHTML = keys[contador];
   button.style.width = "40px";
@@ -90,17 +90,17 @@ var words = [
   "turma da monica",
   "the wichter",
   "friends",
-  "Apenas Um Show",
+  "apenas um show",
   "turma da monica",
-  "Jão Pessoa",
+  "jao pessoa",
   "github",
   "murder in my mind",
   "land of the fire",
-  "Pirates",
-  "Lazaro Talarico",
-  "Ocean",
-  "Terra",
-  "Montanha",
+  "pirates",
+  "lazaro talarico",
+  "ocean",
+  "terra",
+  "montanha",
 ];
 
 var contwords = words.length;
@@ -136,33 +136,68 @@ if (wordtype == true) {
   word[0] = wordspace[0].split("");
 }
 
+var quantidadeLetras = 0;
+
 for (var i = 0; i < word.length; i++) {
   for (var j = 0; j < word[i].length; j++) {
     var input = document.createElement("input");
     input.setAttribute("type", "text");
     input.setAttribute("class", "form-control");
     input.setAttribute("value", word[i][j]);
-    input.setAttribute("id", "input");
+    input.setAttribute("id", quantidadeLetras);
     input.style.color = "white";
     document.getElementById("word").appendChild(input);
-
-    
+    quantidadeLetras += 1;
   }
   if (word[i].length > 1) {
     var espaco = document.createElement("div");
     espaco.setAttribute("class", "espaco");
     document.getElementById("word").appendChild(espaco);
-
   }
 }
 
 // document.getElementById("in").innerHTML = resultados
 
-var letter
-
-function getValueBotao(id){
+function getValueBotao(id) {
   // letter = document.getElementById("").innerHTML
-  var valueButton = id
-  alert(valueButton)
- 
+  var valueButton = id;
+
+  verificar(valueButton);
+
+  verificarErros();
 }
+
+var quatidadeErros = 0;
+var quantidadeTentativas = 6
+
+function verificar(valueButton) {
+  var quantidadeAcertos = 0;
+  for (var i = 0; i <= quantidadeLetras; i++) {
+    if (valueButton == document.getElementById(i).value) {
+      document.getElementById(i).style.color = "black";
+       quantidadeAcertos = quantidadeAcertos + 1
+       alert("vc acertoo")
+    } 
+    }
+    if(quantidadeAcertos == 0) {
+      alert("vc erro")
+      quatidadeErros += 1 
+    }
+    
+    // if(quantidadeErros >  1){
+    //   document.getElementById("head2").style.visibility = "visible"
+    // }
+    // else if(quantidadeErros)
+  }
+
+  function verificarErros(){
+    for(var i=0 ;i  <= quatidadeErros;i++){
+      document.getElementById("p"+ i).style.visibility = "visible"
+    } 
+    if(quatidadeErros = 6){
+     alert("Game Over")
+    }
+  
+  }
+    
+
